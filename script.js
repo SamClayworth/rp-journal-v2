2b644be1bbb3fc2d936ca6137b1ef4d11702bef4
SamClayworth <clayworth6@hotmail.com>
2023-11-01 14:24:08 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Make variables for the inputs
+
+// Create a Contact Class with Name, Box#, Job, Details
+
+// create a function called AddContact
+
+// Inside function, create an empty contactList array to hold our Contact objects
+
+// inside function, call a render function that will display the contacts in our DOM
